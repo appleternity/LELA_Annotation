ae0b8d860ba69fee8ced9ca84f1073509cccede7
appleternity <b99901034@ntu.edu.tw>
2020-04-12 00:56:57 -0400

    finished first version

--- annotation_use/main.js
+++ annotation_use/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     var update_num = function(panel_id) {
         console.log("#"+panel_id+" .selected");
         var length = $("#"+panel_id+" .selected").length;
@@ -34,6 +33,24 @@ $(document).ready(function() {
         button.removeClass("btn-danger").addClass("btn-info");
     });
 
+    $(document).on("change", "#reason_table input[type='checkbox']", function(evt) {
+        var target = $(evt.target);
+        if (this.checked) {
+            target.parents("tr").find("button").removeClass("btn-danger btn-info").addClass("btn-secondary").attr("disabled", true).text("Select a Reason");
+            
+            // Add information to the stage 3
+            var sent_id = target.parents("tr").attr("sent_id");
+            $("#"+sent_id).addClass("accept");
+
+        } else { 
+            target.parents("tr").find("button").removeClass("btn-secondary").addClass("btn-danger").attr("disabled", false);
+            
+            // Add information to the stage 3
+            var sent_id = target.parents("tr").attr("sent_id");
+            $("#"+sent_id).removeClass("accept");
+        }
+    });
+
     $(document).on("click", "#submit_btn", function(evt) {
         $("#warning").text("");
 
@@ -56,21 +73,56 @@ $(document).ready(function() {
         $("#mturk_form").submit();
     });
 
-    $(document).on("click", "#next_btn", function() {
+    $(document).on("click", "#next_btn", function(evt) {
+        var stage = parseInt($(evt.target).attr("stage"));
         $("#warning").text("");
-        var answers = $("#question_table .form-radio:checked");
-        if (answers.length != 20) {
-            $("#warning").text("Please finish all the questions! ( "+ (20 - answers.length) +" left)");
-            return;
-        }
-        var text_answer = JSON.stringify($.map(answers, function(e) { e = $(e); return {"id":e.attr("name"), "ans":e.val()} }));
-        $("#fib_answer").val(text_answer);
-        $("#next_btn").hide();
-        $("#first_stage").hide();
-        $("#submit_btn").show();
-        $("#second_stage").css("display", "flex");
 
-    })
+        // Stage 1
+        if (stage == 1) {
+            // check number of answers
+            var answers = $("#question_table .form-radio:checked");
+            if (answers.length != 20) {
+                $("#warning").text("Please finish all the questions! ( "+ (20 - answers.length) +" left)");
+                return;
+            }
+
+            // change to stage 2
+            var text_answer = JSON.stringify($.map(answers, function(e) { e = $(e); return {"id":e.attr("name"), "ans":e.val()} }));
+            $("#fib_answer").val(text_answer);
+            $("#first_stage").hide();
+            $("#second_stage").show();
+            $("#next_btn").attr("stage", "2");
+
+        // Stage 2
+        } else if (stage == 2) {
+            // check number of answers
+            var id_list = $.map($("#reason_table tbody tr"), function(e, index) {return $(e).attr("sent_id");});
+            var check_list = $.map($("#reason_table input[type='checkbox']"), function(e, index) {return e.checked;});
+            var reason_list = $.map($("#reason_table button"), function(e, index) {return $(e).text().trim();});
+            console.log(id_list, check_list, reason_list);
+
+            var answer = [];
+            for (var i = 0; i < id_list.length; i++) {
+                if (check_list[i] == false && reason_list[i] == "Select a Reason") {
+                    $("#warning").text("Please finish all the questions!");
+                    return;
+                }
+                answer.push({
+                    "id": id_list[i],
+                    "accept": check_list[i],
+                    "reason": reason_list[i],
+                });
+            }
+
+            // change to stage 3
+            $("#reason_answer").val(JSON.stringify(answer));
+            $("#second_stage").hide();
+            $("#third_stage").css("display", "flex");
+            $("#next_btn").hide();
+            $("#submit_btn").show();
+        }
+    });
+
 
 });
 
